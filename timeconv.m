143f0f96d5797490a8ce24e43b1e2c627f00f5f1
James Tralie <jtralie@celaeno4.Princeton.EDU>
2017-07-26 14:16:45 -0400

    Comments added to explain input/output of function

--- timeconv.m
+++ timeconv.m
@@ -1,16 +1,21 @@
 function gnss_times=timeconv(weeknums,timeofweeks)
 % gnss_times=timeconv(weeknums,timeofweeks)
 % 
-% Time conversions of what to what
+% Time conversion for GPS data from number of weeks since Jan 6, 1980 
+% and time elapsed (ms) since the beginning of the week (Sunday 00:00:00). 
+% The output is in a GNSS timestamp (seconds) format. 
+%
+% This output can then be given to gnss_datevec.m to convert to a 
+% [yyyy mm dd HH MM SS] format. 
 %
 % INPUT:
 %
-% weeknums           Is what
-% timeofweeks        Is what?
+% weeknums          number of weeks since Jan 6, 1980
+% timeofweeks       time elapsed (ms) since beginning of week (Sunday)
 %
 % OUTPUT:
 %
-% gnss_times         Is what?
+% gnss_times         GNSS timestamp (seconds) 
 %
 % Last modified by jtralie@princeton.edu on 07/26/2017
 
