88c76bcdfed287be199957087f379a0e331bb48e
Frederik J. Simons <fjsimons@gmail.com>
2017-07-26 12:07:59 -0400

    Additional functionality and bug fixes

--- timeconv.m
+++ timeconv.m
@@ -1,3 +1,17 @@
-%% Time conversions
-function gnss_times=timeconv(weeknums,timeofweeks);
+function gnss_times=timeconv(weeknums,timeofweeks)
+% gnss_times=timeconv(weeknums,timeofweeks)
+% 
+% Time conversions of what to what
+%
+% INPUT:
+%
+% weeknums           Is what
+% timeofweeks        Is what?
+%
+% OUTPUT:
+%
+% gnss_times         Is what?
+%
+% Last modified by jtralie@princeton.edu on 07/26/2017
+
 gnss_times = gadd(timeofweeks,gmultiply(weeknums,604800)); 
