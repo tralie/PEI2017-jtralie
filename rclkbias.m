4f49b1b1f0de99c7f3c9f77ece1e4d0c9d706049
tralie <jtralie@princeton.edu>
2017-08-04 11:01:39 -0400

    updated the commenting for this function

--- rclkbias.m
+++ rclkbias.m
@@ -1,5 +1,28 @@
 function [rclk] = rclkbias(PVTGeoFileName)
-PVTGeoFileName = 'E:\Princeton\Sophomore\Internships\PEI\PVTSatCartesian\pton1900.17__SBF_PVTGeodetic2.txt';
+% [rclk] = rclkbias(PVTGeoFileName)
+% 
+% Reads out the receiver clock bias at 1 hour increments in a specified
+% PVTGeodetic2 file (converted SBF to ASCII using bin2asc)
+%
+% INPUT:
+%
+% PVTGeoFileName      The PVTGeodetic2 file returned from a bin2asc
+%                     conversion of SBF files
+%
+% OUTPUT:
+%
+% rclk                The receiver clock bias (s) for a given PVTGeodetic2 file 
+%
+% EXAMPLE:
+% Complete a bin2asc conversion on an SBF file to retrieve a
+% PVTGeodetic2 file. Below is an example file
+% returned when I converted a file pton1900.17_ from SBF to ASCII using
+% bin2asc: 
+% PVTGeoFileName = 'pton1900.17__SBF_PVTGeodetic2.txt';
+% I have included this file in my github repository 'PEI2017-jtralie' for use and
+% demo purposes. 
+% 
+% Last modified by jtralie@princeton.edu on 08/04/2017
 delimiter = ',';
 formatSpec = '%f%s%s%s%s%s%s%s%s%s%s%s%s%s%f%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%[^\n\r]';
 fileID = fopen(PVTGeoFileName,'r');
